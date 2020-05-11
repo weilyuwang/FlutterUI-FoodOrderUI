19438ac26a6a531d06b0810cdde9042cda27211d
weilyuwang <weilyuwang@gmail.com>
2020-05-11 12:19:47 -0400

    implement appBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../data/data.dart';
+
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -8,6 +10,27 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.account_circle),
+          iconSize: 30.0,
+          onPressed: () {},
+        ),
+        title: Text('Food Delivery'),
+        actions: <Widget>[
+          FlatButton(
+            child: Text(
+              'Cart (${currentUser.cart.length})',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+              ),
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
