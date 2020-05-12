42bf7a5c2b8ea44ea16da514184efaad5102bdc2
weilyuwang <weilyuwang@gmail.com>
2020-05-11 23:55:12 -0400

    implement bottom sheet checkout button

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../data/data.dart';
+import '../data/data.dart';
 import '../models/order.dart';
 
 class CartScreen extends StatefulWidget {
@@ -106,11 +107,14 @@ class _CartScreenState extends State<CartScreen> {
               ],
             ),
           ),
-          Text(
-            '\$${order.quantity * order.food.price}',
-            style: TextStyle(
-              fontSize: 16.0,
-              fontWeight: FontWeight.w600,
+          Container(
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              '\$${order.quantity * order.food.price}',
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w600,
+              ),
             ),
           )
         ],
@@ -120,16 +124,71 @@ class _CartScreenState extends State<CartScreen> {
 
   @override
   Widget build(BuildContext context) {
+    double totalPrice = 0;
+    currentUser.cart.forEach(
+      (Order order) => totalPrice += order.quantity * order.food.price,
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Cart (${currentUser.cart.length})'),
       ),
       body: ListView.separated(
-        itemCount: currentUser.cart.length,
+        itemCount: currentUser.cart.length + 1,
         itemBuilder: (BuildContext context, int index) {
-          Order order = currentUser.cart[index];
-
-          return _buildCartItem(order);
+          if (index < currentUser.cart.length) {
+            Order order = currentUser.cart[index];
+            return _buildCartItem(order);
+          }
+          // total count and cost
+          return Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Estimated Delivery Time',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      '25 min',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Total Cost:',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      '\$${totalPrice.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        color: Colors.green[700],
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 80.0),
+              ],
+            ),
+          );
         },
         separatorBuilder: (BuildContext context, int index) {
           return Divider(
@@ -137,6 +196,33 @@ class _CartScreenState extends State<CartScreen> {
             height: 1.0,
           );
         },
+      ),
+      bottomSheet: Container(
+        height: 100.0,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 6.0,
+              color: Colors.black26,
+              offset: Offset(0, -1),
+            ),
+          ],
+        ),
+        child: Center(
+          child: FlatButton(
+            child: Text(
+              'CHECKOUT',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: () {},
+          ),
+        ),
       ),
     );
   }
